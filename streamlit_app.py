8dbc8d73f7e9a78683d52e1766f1e8bef0617856
saki1702 <114433713+saki1702@users.noreply.github.com>
2022-09-30 22:48:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,8 @@ streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & blueberry Oatmeal')
+
+streamlit.text('Kale, Spinach & Rocket smoothie')
+
+
+streamlit.text('Hard-Boiled Free-Range Egg')
