deb1e6e1adc97c9ec00dfcefbb094af0d3ddead1
saki1702 <114433713+saki1702@users.noreply.github.com>
2022-10-11 23:10:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,5 +67,5 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("What fruit would you like to add?")
-streamlit.text(add_my_fruit, "Jackfruit")
+streamlit.text("Jackfruit")
 
