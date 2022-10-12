66ae322d721a096bab5ba6ea9212454572a9ec15
saki1702 <114433713+saki1702@users.noreply.github.com>
2022-10-12 10:36:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,8 +66,6 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
-streamlit.text("What fruit would you like to add?")
-streamlit.text("Jackfruit")
 fruit_choice = streamlit.text_input (("What fruit would you like to add?"))
 streamlit.write("Thanks for adding Jackfruit")
 
