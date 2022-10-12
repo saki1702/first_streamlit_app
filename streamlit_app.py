4a466b3b003ac07f442cb1dc89545d1cd251545b
saki1702 <114433713+saki1702@users.noreply.github.com>
2022-10-12 11:11:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,11 +48,19 @@ streamlit.text(fruityvice_response)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#streamlit.write('The user entered ', fruit_choice)
 
-import requests
-Fruityvice_response = requests.get ("https://fruityvice.com/api/fruit/kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+  streamlit.error("Please select a fruit to get information")
+  else:
+    Fruityvice_response = requests.get ("https://fruityvice.com/api/fruit/" = fruit_choice)
+  
+#streamlit.write('The user entered ', fruit_choice)
+
+#import requests
+#Fruityvice_response = requests.get ("https://fruityvice.com/api/fruit/kiwi")
 
 
 # write your own comment -Normalize the advice
