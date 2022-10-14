e0dc6bbf9664e5f0ddb008ac024fca77e7f4a9ff
saki1702 <114433713+saki1702@users.noreply.github.com>
2022-10-14 17:48:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,8 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit for more information.")
   else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-        streamlit.dataframe(back_from_function)
+       back_from_function = get_fruityvice_data(fruit_choice)
+       streamlit.dataframe(back_from_function)
 
       
 except URLError as e:
