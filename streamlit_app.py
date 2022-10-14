e3ccd39290976865a6f4e5826a955411ec3c1fb5
saki1702 <114433713+saki1702@users.noreply.github.com>
2022-10-14 15:33:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,19 +66,29 @@ try:
 
 # write your own comment - simple language return
 #dont run anything here while we troubleshoot
-#streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
+#snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * from fruit_load_list")
+        return my_cur.fetchall()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+ 
+    #add a button to load the fruit
+
+
+if streamlit.button('Get fruit load list');
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+
+streamlit.stop()
+
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+
 fruit_choice = streamlit.text_input (("What fruit would you like to add?"))
 streamlit.write('Thanks for adding', fruit_choice)
 
